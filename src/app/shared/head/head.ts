c60d9eadfee463c8441ed00aa7cbeabe413abc7d
JuanECK <juan_eck@hotmail.com>
2025-07-04 00:51:14 -0600

    diseño de perfil y cierre de sesion

--- src/app/shared/head/head.ts
+++ src/app/shared/head/head.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AuthService } from '../../core/service/authService.service';
 
 @Component({
@@ -12,8 +12,18 @@ constructor(
   private authService:AuthService
 ){}
 
+@ViewChild('drop') drop!:ElementRef
+
+// opcion para desplegarlo con un clic
+dropdown(){
+
+//   this.drop.nativeElement.classList.toggle('mostrar')
+//   console.log(this.drop.nativeElement)
+
+}
+
 logOut(){
-  // this.authService.logOut();
+  this.authService.logOut();
 }
 
 }
